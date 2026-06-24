e3fb88718df6c79da555e6e49361fcd6feabc9f6
a20791318-glitch <a20791318@gmail.com>
2026-06-24 21:58:10 +0300

    Add files via upload

--- states/__init__.py
+++ states/__init__.py
@@ -1,1 +1,48 @@
+from aiogram.fsm.state import State, StatesGroup
 
+
+class IncomeState(StatesGroup):
+    cash_amount = State()
+    card_amount = State()
+    currency_amount = State()
+
+
+class ExpenseState(StatesGroup):
+    add_category = State()
+    expense_amount = State()
+
+
+class DebtTakeState(StatesGroup):
+    person = State()
+    amount = State()
+    comment = State()
+
+
+class DebtGiveState(StatesGroup):
+    person = State()
+    amount = State()
+    comment = State()
+
+
+class DebtPayState(StatesGroup):
+    amount = State()
+
+
+class DebtReturnState(StatesGroup):
+    amount = State()
+
+
+class SupportState(StatesGroup):
+    message = State()
+
+
+class SupportReplyState(StatesGroup):
+    message = State()
+
+
+class BroadcastState(StatesGroup):
+    message = State()
+
+
+class RestoreState(StatesGroup):
+    file = State()
